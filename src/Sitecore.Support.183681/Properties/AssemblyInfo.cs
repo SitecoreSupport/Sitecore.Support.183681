b910451c9f20e4ec4df4f3b5c15f514ac64797cc
Ivan Sheyenko <ivs@sitecore.net>
2017-09-13 16:24:08 +0300

    patch created

---
+++ src/Sitecore.Support.183681/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.183681")]
+[assembly: AssemblyProduct("Sitecore.Support.183681")]
+[assembly: ComVisible(false)]
